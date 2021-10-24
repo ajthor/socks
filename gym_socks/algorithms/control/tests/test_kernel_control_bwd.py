8eda94bca584ab306c33d951fcd3c73fee6d1080
adam <adam.thorpe.g@gmail.com>
2021-10-24 16:24:40 -0600

    Temporarily removing algorithm test cases.

--- gym_socks/algorithms/control/tests/test_kernel_control_bwd.py
+++
@@ -1,8 +0,0 @@
-import unittest
-
-import gym_socks
-
-
-class TestKernelControlBwd(unittest.TestCase):
-    def test_pass(cls):
-        cls.assertTrue(True)
--- gym_socks/algorithms/reach/__init__.py
+++ gym_socks/algorithms/reach/__init__.py
@@ -5,7 +5,6 @@ __all__ = [
     "stochastic_reachability",
 ]
 
-from gym_socks.algorithms.reach.kernel_control_maximal import KernelControlMaximal
 from gym_socks.algorithms.reach.kernel_sr_max import KernelMaximalSR
 from gym_socks.algorithms.reach.kernel_sr_max import kernel_sr_max
 from gym_socks.algorithms.reach.kernel_sr import KernelSR
